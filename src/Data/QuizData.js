dae00ec59906de8c0fd23debb03d2c4b8cbc9e87
mansi-jaimini <mansijaimini31@gmail.com>
2024-07-30 22:43:52 +0530

    react-quiz-app

---
+++ src/Data/QuizData.js
@@ -0,0 +1,33 @@
+export const QuizData = [
+    {
+        question: "Which language runs in a web browser?",
+        options:[ "Java", "C", "Python", "JavaScript"],
+        answer: 4
+    },
+    {
+        question: "What does CSS stand for?",
+        options:["Central Style Sheets", "Cascading Style Sheets", "Cascading Simple Sheets", "Cars SUVs Sailboats"],
+        answer: 2
+    },
+    {
+        question: "What does HTML stand for?",
+        options: ["Hypertext Markup Language", "Hypertext Markdown Language", "Hyperloop Machine Language", "Helicopters Terminals Motorboats Lamborginis"],
+        answer: 1
+    },
+    {
+        question: "What year was JavaScript launched?",
+        options: ["1996", "1995", "1994", "none of the above"],
+        answer:2
+    },
+    {
+        question: "Which of the following is NOT a valid HTML tag?",
+        options :["<header>","<section>","<paragraph>","<footer>"],
+        answer: 3
+    },
+    {
+        question: "Which Front-End framework is developed by Facebook and is used for building user interfaces?",
+        options :["Angular","React","Vue.js","jQuery"],
+        answer: 2
+    },
+
+];
